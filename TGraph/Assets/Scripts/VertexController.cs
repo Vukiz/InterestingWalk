eb6ba83259a109b92fa2eebbe534ebad2c4a7b3a
Eugene Alekseev <vukiz@mail.ru>
2017-09-17 00:56:30 +0300

    Update

---
+++ TGraph/Assets/Scripts/VertexController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class VertexController : MonoBehaviour
+    {
+        public int Interest;
+        public List<EdgeController> ConjoinedEdges;
+        public VertexState CurrentState;
+        public int DistanceFromStart = 0;
+        public int Depth;
+
+        public enum VertexState 
+        {
+            Unvisited,
+            Visited
+        }
+        public void RandomizeInterest()
+        {
+            Interest = Random.Range(1, 10);
+            GetComponentInChildren<TextMesh>().text = Interest.ToString();
+        }
+
+        public List<VertexController> GetAdjacentVertices()
+        {
+            return ConjoinedEdges.Select(edge => (edge.First != this) ? edge.First : edge.Second).ToList();
+        }
+    }
+}
